e98490be5ef73e33d14c652693d10b6f8e5329d3
PetrisCy <Peter_-05@hotmail.com>
2024-06-02 20:21:05 +0300

    fix

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,30 +1,51 @@
+import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+
 function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="nav">
-      <div className="logo">
-        <a href="#">Logo</a>
+    <>
+      <div className="nav">
+        <div className="logo">
+          <a href="#">Logo</a>
+        </div>
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <a href="#">Search</a>
+          </li>
+          <li>
+            <a href="#">Cart</a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a href="#">Shop</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="#">Search</a>
-        </li>
-        <li>
-          <a href="#">Cart</a>
-        </li>
-      </ul>
-    </div>
+    </>
   );
 }
 
 export default Navigation;
+
+{
+  /* <FaTimes />
+<FaBars /> */
+}
